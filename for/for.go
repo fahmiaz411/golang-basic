ba370a8bca51a80ba1c09748af4bc7a8b9ac60c5
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-08 11:17:03 +0700

    for while

--- for/for.go
+++ for/for.go
@@ -2,25 +2,49 @@ package main
 
 import "fmt"
 
-func main() {
-	var arr = []string{
-		"hello",
-		"hey",
-		"ello",
-	}
-
-	for i := 0; i < len(arr); i++ {
+func BreakContinueFor (array []string) {
+	defer fmt.Println("")
+	for i := 0; i < len(array); i++ {
 		if i == 0 {
 			continue
 		}
-		fmt.Println(arr[i])
+		fmt.Println(array[i])
 		
 		if i == 1 {
 			break
 		}
 	}
+}
 
-	for _, data := range arr {
+func RangeFor(array []string){
+	defer fmt.Println("")
+	for _, data := range array {
 		fmt.Println(data)
 	}
+}
+
+func WhileFor(array []string){
+	defer fmt.Println("")
+	loop := 0
+	for {
+		if loop < len(array){
+			fmt.Println(array[loop])
+			loop++
+		} else {
+			break
+		}
+	}
+}
+
+func main() {
+	var arr = []string{
+		"hello",
+		"hey",
+		"l",
+	}
+
+	BreakContinueFor(arr)
+	RangeFor(arr)
+	WhileFor(arr)
+	
 }
